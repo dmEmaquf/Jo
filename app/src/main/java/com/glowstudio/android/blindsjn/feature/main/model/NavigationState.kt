459964eb072748ec344d294e037bbbbdbcf3cf62
gyshim <gyshim1004@gmail.com>
2025-05-31 03:57:17 +0900

    게시판 목록 -> 커뮤니티

--- app/src/main/java/com/glowstudio/android/blindsjn/feature/main/model/NavigationState.kt
+++ app/src/main/java/com/glowstudio/android/blindsjn/feature/main/model/NavigationState.kt
@@ -17,7 +17,7 @@ data class NavigationState(
 
 sealed class Screen(val route: String, val title: String, val icon: ImageVector) {
     object Home : Screen("home", "홈화면", Icons.Default.Home)
-    object Posts : Screen("board", "게시판 목록", Icons.Default.GridView)
+    object Posts : Screen("board", "커뮤니티", Icons.Default.Forum)
     object Popular : Screen("paymanagement", "매출 관리", Icons.Default.MonetizationOn)
     object Margin : Screen("foodcoast", "마진 관리", Icons.Default.Receipt)
     object Messages : Screen("message", "캘린더", Icons.Default.CalendarToday)
