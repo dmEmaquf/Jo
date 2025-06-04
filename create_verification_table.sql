44641fe9dafab5cda2e6834c007411f06fa1e9c8
dmEmaquf <wgysuarang@gmail.com>
2025-06-04 16:53:39 +0900

    수정

--- create_verification_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS verification_codes (
-    phone VARCHAR(20) PRIMARY KEY,
-    code VARCHAR(6) NOT NULL,
-    created_at DATETIME NOT NULL,
-    verified BOOLEAN DEFAULT FALSE,
-    INDEX idx_phone (phone)
-); 
